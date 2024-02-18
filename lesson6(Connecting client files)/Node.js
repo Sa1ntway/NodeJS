bff2a42b35837b40d3ac2d8ab61abb2a9ffd082a
Saintway <fryyystandoff@gmail.com>
2024-02-18 12:10:40 +0300

    123

---
+++ lesson6(Connecting client files)/Node.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const fs = require("fs");
+http
+  .createServer(function (request, response) {
+    if (request.url.endsWith(".html")) {
+      addClientFile(request.url, "text/html", response);
+    } else if (request.url.endsWith(".css")) {
+      addClientFile(request.url, "text/css", response);
+    } else if (request.url.endsWith(".js")) {
+      addClientFile(request.url, "text/javascript", response);
+    } else if (request.url.endsWith(".jpg")) {
+      addClientFile(request.url, "image/jpg", response);
+    } else if (request.url.endsWith(".png")) {
+      addClientFile(request.url, "image/png", response);
+    } else {
+    }
+  })
+  .listen(8888);
+function addClientFile(url, header, response, err) {
+  let fileUrl = url.slice(1);
+  //   let header =
+  fs.readFile(fileUrl, (err, data) => {
+    response.setHeader("Content-type", header);
+    response.statusCode = 200;
+    response.write(data);
+    response.end();
+    if (err) {
+      fs.readFile("404.html", (err, data) => {});
+    }
+  });
+}
